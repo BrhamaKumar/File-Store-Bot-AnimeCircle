7a03c6269585a3540ad8eb4e03c063a3a2b61f31
BrhamaKumar <146445615+BrhamaKumar@users.noreply.github.com>
2024-01-31 19:43:10 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -8,14 +8,14 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-                        text = f"<b>About this Bot:\n\n  A Telegram Bot for storing posts or files that can be accessed via a Special Link.\n\n👨‍💻 Modified by @Shidoteshika1</b>",
+                        text = f"<b>About this Bot:\n\n  A Telegram Bot for storing posts or files that can be accessed via a Special Link.\n\n👨‍💻 Modified by @Contactpkbot</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("○ My Owner ○", url = "https://t.me/Shidoteshika1")
+                        InlineKeyboardButton("○ My Owner ○", url = "https://t.me/Contactpkbot")
                     ],[
-                        InlineKeyboardButton('Aɴɪᴍᴇ Cʜᴀɴɴᴇʟ', url = 'https://t.me/Animemoviesr'),
+                        InlineKeyboardButton('Movie Group', url = 'https://t.me/funnyzilla'),
 InlineKeyboardButton('Aɴɪᴍᴇ Gʀᴏᴜᴘ', url = 'https://t.me/ChatBox480')
                     ],[
                         InlineKeyboardButton("🔒 Close 🔒", callback_data = "close")
