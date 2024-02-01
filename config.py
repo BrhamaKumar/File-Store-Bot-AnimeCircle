2046a6c168d53627ad1d9bc029362c7eae647433
BrhamaKumar <146445615+BrhamaKumar@users.noreply.github.com>
2024-02-01 15:22:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,17 @@
 import os
 import logging
 from logging.handlers import RotatingFileHandler
+import re
+from os import environ, getenv
+
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
 
 
 
