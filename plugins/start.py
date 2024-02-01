28db882611d0d4d2e00c341134e175d00fae035d
BrhamaKumar <146445615+BrhamaKumar@users.noreply.github.com>
2024-02-01 19:24:44 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,4 +1,23 @@
 #(©)CodeXBotz
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+
+from config import Config
+# the Strings used for this "thing"
+from translation import Translation
+from utils import verify_user, check_token
+from pyrogram import filters, enums
+from database.adduser import AddUser
+from pyrogram import Client
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+from bot import Bot
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
 
 
 
@@ -137,6 +156,29 @@ async def not_joined(client: Client, message: Message):
             reply_markup=InlineKeyboardMarkup(btn)
         )
         return
+
+    data = update.command[1]
+
+    if data.split("-", 1)[0] == "verify":
+        userid = data.split("-", 2)[1]
+        token = data.split("-", 3)[2]
+        if str(update.from_user.id) != str(userid):
+            return await update.reply_text(
+                text="<b>ᴇxᴘɪʀᴇᴅ ʟɪɴᴋ ᴏʀ ɪɴᴠᴀʟɪᴅ ʟɪɴᴋ !</b>",
+                protect_content=True
+            )
+        is_valid = await check_token(bot, userid, token)
+        if is_valid == True:
+            await update.reply_text(
+                text=f"<b>ʜᴇʟʟᴏ {update.from_user.mention} 👋,\nʏᴏᴜ ᴀʀᴇ sᴜᴄᴄᴇssғᴜʟʟʏ ᴠᴇʀɪғɪᴇᴅ !\n\nɴᴏᴡ ʏᴏᴜ ʜᴀᴠᴇ ᴜɴʟɪᴍɪᴛᴇᴅ ᴀᴄᴄᴇss ғᴏʀ ᴀʟʟ ᴜʀʟ ᴜᴘʟᴏᴀᴅɪɴɢ ᴛɪʟʟ ᴛᴏᴅᴀʏ ᴍɪᴅɴɪɢʜᴛ.</b>",
+                protect_content=True
+            )
+            await verify_user(bot, userid, token)
+        else:
+            return await update.reply_text(
+                text="<b>ᴇxᴘɪʀᴇᴅ ʟɪɴᴋ ᴏʀ ɪɴᴠᴀʟɪᴅ ʟɪɴᴋ !</b>",
+                protect_content=True
+            )
     
     
     buttons = [
